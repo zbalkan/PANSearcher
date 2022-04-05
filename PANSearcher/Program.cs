ec768c0f1e508e58db0beaf98f63fe3ca18611b4
Zafer Balkan <zafer@zaferbalkan.com>
2022-04-05 12:41:18 +0300

    Removed interactive UI and added search base option

--- PANSearcher/Program.cs
+++ PANSearcher/Program.cs
@@ -1,20 +1,16 @@
-﻿using Utility.CommandLine;
+﻿using System.Diagnostics;
+using Utility.CommandLine;
 
 namespace PANSearcher
 {
     public static class Program
     {
         /// <summary>
-        ///     Gets or sets the list of Card numbers.
+        ///     Gets or sets the base search directory.
         /// </summary>
-        [Argument('c', "CardNumbers", "Type in cardnumbers delimited by comma.")]
-        private static string[]? CardNumbers { get; set; }
+        [Argument('s', "SEARCH", @"Base directory to search in (default: C:\)")]
+        private static string? SearchBase { get; set; }
 
-        /// <summary>
-        ///     Gets or sets the FilePath.
-        /// </summary>
-        [Argument('f', "FilePath", "Path to the file to be checked against PAN numbers.")]
-        private static string? FilePath { get; set; }
 
         /// <summary>
         ///     Displays help text.
@@ -38,7 +34,11 @@ namespace PANSearcher
         public static void Main(string[] args)
         {
             // enable ctrl+c
-            Console.CancelKeyPress += (o, e) => Environment.Exit(1);
+            Console.CancelKeyPress += (o, e) =>
+            {
+                Console.WriteLine("Operation cancelled by user.");
+                Environment.Exit(1);
+            };
 
             Arguments.Populate();
 
@@ -48,100 +48,71 @@ namespace PANSearcher
                 return;
             }
 
-            if (!string.IsNullOrEmpty(FilePath) && CardNumbers != null && CardNumbers.Length > 0)
+            if (string.IsNullOrEmpty(SearchBase))
             {
-                Console.WriteLine("You cannot provide both file path and card numbers at the same time.");
-                Environment.Exit(1);
+                SearchBase = @"C:\";
             }
 
-            // Interactive session
-            if (string.IsNullOrEmpty(FilePath) && (CardNumbers == null || CardNumbers.Length == 0))
+            var files = new List<string>();
+
+            var options = new EnumerationOptions
             {
-                while (true)
+                IgnoreInaccessible = true,
+                RecurseSubdirectories = true
+            };
+
+            // TODO: Use config file to search for extensions.
+            files = Directory.EnumerateFiles(SearchBase, "*.txt", options).ToList();
+
+            Console.WriteLine(@$"Found {files.Count} text file(s) under {SearchBase}");
+
+            var fileCounter = 0;
+            foreach (var file in files)
+            {
+                IEnumerable<string>? lines = null;
+
+                try
                 {
-                    Console.Write("Enter Card Number or type Q or q to exit : ");
-                    var input = Console.ReadLine();
-                    if (string.IsNullOrEmpty(input))
+                    lines = File.ReadLines(file, System.Text.Encoding.UTF8);
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message);
+                    continue;
+                }
+
+                var increment = false;
+                foreach (var line in lines)
+                {
+                    var found = PAN.ParseLine(line);
+                    if (found.Count == 0)
                     {
-                        Console.WriteLine("Invalid input.");
                         continue;
                     }
-                    if (input.ToUpper().Equals("Q", StringComparison.Ordinal))
-                    {
-                        break;
-                    }
-                    var cardType = PAN.GetCardType(input);
-                    if (cardType != CardType.Invalid && Luhn.Validate(input))
-                    {
-                        Console.WriteLine($"Valid {Enum.GetName(typeof(CardType), cardType)} Card.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid Card");
-                    }
 
-                }
-            }
-            else
-            {
-                if (!string.IsNullOrEmpty(FilePath))
-                {
-                    var lines = File.ReadLines(FilePath);
-                    foreach (var line in lines)
+                    foreach (var item in found)
                     {
-                        var found = PAN.ParseLine(line);
-                        if (found.Count == 0)
+                        var cardType = PAN.GetCardType(item);
+                        if (cardType == CardType.Invalid)
                         {
                             continue;
                         }
 
-                        foreach (var item in found)
+                        if (cardType != CardType.Invalid && Luhn.Validate(item))
                         {
-                            var cardType = PAN.GetCardType(item);
-                            if (cardType == CardType.Invalid)
-                            {
-                                continue;
-                            }
-
-                            if (cardType != CardType.Invalid && Luhn.Validate(item))
-                            {
-                                string pan;
-                                if (Truncate) { pan = PAN.Truncate(item); }
-                                else if(Unmask) { pan = item; }
-                                else { pan = PAN.Mask(item); }
-                                Console.WriteLine($"FOUND PAN: {pan} - {Enum.GetName(typeof(CardType), cardType)} (Path: {FilePath})");
-                            }
-                            else
-                            {
-                                // Console.WriteLine("Invalid Card");
-                            }
+                            string pan;
+                            if (Truncate) { pan = PAN.Truncate(item); }
+                            else if (Unmask) { pan = item; }
+                            else { pan = PAN.Mask(item); }
+                            Console.WriteLine($"FOUND PAN: {pan} - {Enum.GetName(typeof(CardType), cardType)} (Path: {file})");
+                            increment = true;
                         }
                     }
                 }
-                else
-                {
-                    if (CardNumbers != null) // I know that it is not null but I needed to silence the warning
-                    {
-                        foreach (var item in CardNumbers)
-                        {
-                            var cardType = PAN.GetCardType(item);
-                            if (cardType == CardType.Invalid)
-                            {
-                                continue;
-                            }
-
-                            if (cardType != CardType.Invalid && Luhn.Validate(item))
-                            {
-                                Console.WriteLine($"{item} - Valid {Enum.GetName(typeof(CardType), cardType)} Card.");
-                            }
-                            else
-                            {
-                                Console.WriteLine($"{item} - Invalid Card");
-                            }
-                        }
-                    }
-                }
+                if (increment) fileCounter++;
             }
+
+            Console.WriteLine($"Total {fileCounter} files found with at leas one PAN number. To ignore the false positives, you can configure to ignore those folders.");
         }
 
         /// <summary>
