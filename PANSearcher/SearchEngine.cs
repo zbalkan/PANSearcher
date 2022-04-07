067891c5b958865b811cb79c64bf82b8185b1ac0
Zafer Balkan <zafer@zaferbalkan.com>
2022-04-07 18:08:00 +0300

    Added tasks for parallel processing.

--- PANSearcher/SearchEngine.cs
+++ PANSearcher/SearchEngine.cs
@@ -1,38 +1,21 @@
-﻿using System.Diagnostics;
-using System.Text;
-
-namespace PANSearcher.Context
+﻿namespace PANSearcher.Context
 {
     public class SearchEngine
     {
         public static void Search(string searchBase, IEnumerable<string> excluded, IEnumerable<string> extensions, IContext context, PANDisplayMode displayMode = PANDisplayMode.Masked)
         {
-            var options = new EnumerationOptions
-            {
-                IgnoreInaccessible = true,
-                RecurseSubdirectories = true
-            };
             var fileCounter = 0;
-
-            foreach (var textFileExt in extensions)
+            var tasks = new Task<int>[extensions.Count()];
+            var collection = extensions.ToArray();
+            for (var i = 0; i < collection.Length; i++)
             {
-                Print.Output($"Started searching for files with '*{textFileExt}' extensions");
-                var files = Directory.EnumerateFiles(searchBase, $"*{textFileExt}", options).Where(f => !IsExcluded(f, excluded));
-
-                var matches = from file in files.AsParallel().AsOrdered().WithMergeOptions(ParallelMergeOptions.NotBuffered)
-                              from line in context.Read(file).Zip(Enumerable.Range(1, int.MaxValue), (s, i) => new { Num = i, Text = s, File = file })
-                              from found in PAN.ParseLine(line.Text)
-                              where (found.Any() && PAN.Validate(found, out var cardType))
-                              select new { line, found };
-
-                foreach (var match in matches)
-                {
-                    _ = PAN.Validate(match.found, out var cardType);
-                    var pan = PAN.Format(match.found, displayMode);
-                    Print.Output($"FOUND PAN: {match.found} - {Enum.GetName(typeof(CardType), cardType)} [{match.line.File} : {match.line.Num}]");
-                }
-
-                fileCounter += matches.GroupBy(m => m.line.File).Count();
+                var extension = collection[i];
+                tasks[i] = new Task<int>(() => RunSearch(searchBase, excluded, context, displayMode, extension));
+                tasks[i].Start();
+            }
+            foreach (var task in tasks)
+            {
+                fileCounter += task.Result;
             }
 
             if (fileCounter == 0)
@@ -43,6 +26,32 @@ namespace PANSearcher.Context
             {
                 Print.Output($"{Environment.NewLine}Total {fileCounter} files found with at least one PAN number. To ignore the false positives, you can configure to ignore those folders.");
             }
+        }
+
+        private static int RunSearch(string searchBase, IEnumerable<string> excluded, IContext context, PANDisplayMode displayMode, string textFileExt)
+        {
+            Print.Output($"Started searching for files with '*{textFileExt}' extensions");
+            var options = new EnumerationOptions
+            {
+                IgnoreInaccessible = true,
+                RecurseSubdirectories = true
+            };
+            var files = Directory.EnumerateFiles(searchBase, $"*{textFileExt}", options).Where(f => !IsExcluded(f, excluded));
+
+            var matches = from file in files.AsParallel().AsOrdered().WithMergeOptions(ParallelMergeOptions.NotBuffered)
+                          from line in context.Read(file).Zip(Enumerable.Range(1, int.MaxValue), (s, i) => new { Num = i, Text = s, File = file })
+                          from found in PAN.ParseLine(line.Text)
+                          where (found.Any() && PAN.Validate(found, out var cardType))
+                          select new { line, found };
+
+            foreach (var match in matches)
+            {
+                _ = PAN.Validate(match.found, out var cardType);
+                var pan = PAN.Format(match.found, displayMode);
+                Print.Output($"FOUND PAN: {match.found} - {Enum.GetName(typeof(CardType), cardType)} [{match.line.File} : {match.line.Num}]");
+            }
+
+            return matches.GroupBy(m => m.line.File).Count();
         }
 
         private static bool IsExcluded(string filePath, IEnumerable<string> excluded)
