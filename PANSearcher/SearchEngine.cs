f4ac8b3be414a75f9946bde46eb06468a1e2e9f3
Zafer Balkan <zafer@zaferbalkan.com>
2022-04-09 13:41:00 +0300

    Moved huce code block of text search in a private method for readability.

--- PANSearcher/SearchEngine.cs
+++ PANSearcher/SearchEngine.cs
@@ -39,79 +39,14 @@ namespace PANSearcher
         private static Report RunSearch(IContext context, string extension)
         {
             var searchBase = Settings.Instance.SearchBase;
-            var excluded = Settings.Instance.ExcludeFolders;
-            var findings = new List<Finding>();
-
             Print.Verbose($"Started searching for files with '*{extension}' extensions");
-            var options = new EnumerationOptions
-            {
-                IgnoreInaccessible = true,
-                RecurseSubdirectories = true
-            };
+
 #pragma warning disable CS8604 // Possible null reference argument.
             var files = SearchFiles(searchBase, $"*{extension}");
 #pragma warning restore CS8604 // Possible null reference argument.
 
-            var matches = from file in files.AsParallel().AsOrdered().WithMergeOptions(ParallelMergeOptions.NotBuffered)
-                          from line in context.Read(file).Zip(Enumerable.Range(1, int.MaxValue), (s, i) => new { Num = i, Text = s, File = file })
-                          from found in PAN.ParseLine(line.Text)
-                          where (found.Any() && PAN.Validate(found, out _))
-                          select new { line, found };
-
-            foreach (var match in matches)
-            {
-                 _ = PAN.Validate(match.found, out var cardType);
-                var pan = PAN.Format(match.found, Settings.Instance.PANDisplayMode);
-                Print.Output($"FOUND PAN: {pan} - {Enum.GetName(typeof(CardType), cardType)} [{match.line.File} : {match.line.Num}]");
-
-                var finding = findings.FirstOrDefault(f => f.FilePath.Equals(match.line.File, StringComparison.OrdinalIgnoreCase));
-                if (finding == null)
-                {
-                    findings.Add(new Finding()
-                    {
-                        FilePath = match.line.File,
-                        Records = new List<Record>()
-                        {
-                            new Record()
-                            {
-                                PossiblePAN = match.found,
-                                CardType = cardType,
-                                LineNumber = match.line.Num,
-                                LineText = match.line.Text
-                            }
-                        }
-                    });
-                }
-                else
-                {
-                    finding.Records.Add(new Record()
-                    {
-                        PossiblePAN = match.found,
-                        CardType = cardType,
-                        LineNumber = match.line.Num,
-                        LineText = match.line.Text
-                    });
-                }
-            }
-            var report = new Report(findings)
-            {
-                NumberOfFiles = files.Count
-            };
+            var report = GenerateSearchResultReport(context, files);
             return report;
-        }
-
-        private static bool IsExcluded(string filePath)
-        {
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
-            for (var i = 0; i < Settings.Instance.ExcludeFolders.Length; i++)
-            {
-                if (filePath.StartsWith(Settings.Instance.ExcludeFolders[i], StringComparison.OrdinalIgnoreCase))
-                {
-                    return true;
-                }
-            }
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
-            return false;
         }
 
         /// <summary> 
@@ -149,6 +84,71 @@ namespace PANSearcher
             }
 
             return files;
+        }
+        private static bool IsExcluded(string filePath)
+        {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            for (var i = 0; i < Settings.Instance.ExcludeFolders.Length; i++)
+            {
+                if (filePath.StartsWith(Settings.Instance.ExcludeFolders[i], StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+            return false;
+        }
+
+        private static Report GenerateSearchResultReport(IContext context, List<string> files)
+        {
+            var findings = new List<Finding>();
+
+            var matches = from file in files.AsParallel().AsOrdered().WithMergeOptions(ParallelMergeOptions.NotBuffered)
+                          from line in context.Read(file).Zip(Enumerable.Range(1, int.MaxValue), (s, i) => new { Num = i, Text = s, File = file })
+                          from found in PAN.ParseLine(line.Text)
+                          where (found.Any() && PAN.Validate(found, out _))
+                          select new { line, found };
+
+            foreach (var match in matches)
+            {
+                _ = PAN.Validate(match.found, out var cardType);
+                var pan = PAN.Format(match.found, Settings.Instance.PANDisplayMode);
+                Print.Output($"FOUND PAN: {pan} - {Enum.GetName(typeof(CardType), cardType)} [{match.line.File} : {match.line.Num}]");
+
+                var finding = findings.FirstOrDefault(f => f.FilePath.Equals(match.line.File, StringComparison.OrdinalIgnoreCase));
+                if (finding == null)
+                {
+                    findings.Add(new Finding()
+                    {
+                        FilePath = match.line.File,
+                        Records = new List<Record>()
+                        {
+                            new Record()
+                            {
+                                PossiblePAN = match.found,
+                                CardType = cardType,
+                                LineNumber = match.line.Num,
+                                LineText = match.line.Text
+                            }
+                        }
+                    });
+                }
+                else
+                {
+                    finding.Records.Add(new Record()
+                    {
+                        PossiblePAN = match.found,
+                        CardType = cardType,
+                        LineNumber = match.line.Num,
+                        LineText = match.line.Text
+                    });
+                }
+            }
+            var report = new Report(findings)
+            {
+                NumberOfFiles = files.Count
+            };
+            return report;
         }
     }
 }
